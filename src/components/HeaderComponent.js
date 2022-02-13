2b9f2a0d908e72d2cc072830926aaf7c5539469b
jesse h <jesselevihall@gmail.com>
2022-02-13 11:40:08 -0700

    button loop animation

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,120 +1,160 @@
-import React, { Component }  from 'react';
-import { Nav, NavbarToggler, Collapse, NavItem, Navbar, NavbarBrand, Jumbotron, Button, Modal, ModalHeader, ModalBody,
-    Form, FormGroup, Input, Label } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
-
+import React, { Component } from "react";
+import {
+  Nav,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Navbar,
+  NavbarBrand,
+  Jumbotron,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
+import { Fade, Loop } from "react-animation-components";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
+    this.toggleNav = this.toggleNav.bind(this);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false,
+    };
+    this.toggleNav = this.toggleNav.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
 
-        this.toggleNav = this.toggleNav.bind(this);
-        this.state = {
-          isNavOpen: false,
-          isModalOpen: false
-        };
-        this.toggleNav = this.toggleNav.bind(this);
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
-    }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
 
-    toggleModal() {
-        this.setState({
-            isModalOpen: !this.state.isModalOpen
-        });
-    }
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
+    });
+  }
 
-    toggleNav() {
-        this.setState({
-            isNavOpen: !this.state.isNavOpen
-        });
-    }
+  handleLogin(event) {
+    alert(
+      `Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`
+    );
+    this.toggleModal();
+    event.preventDefault();
+  }
 
-    handleLogin(event) {
-        alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
-        this.toggleModal();
-        event.preventDefault();
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <Jumbotron fluid>
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <h1>NuCamp</h1>
+                <h2>a better way to camp</h2>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
 
-        render() {
-            return (
-                <React.Fragment>
-                    <Jumbotron fluid>
-                        <div className="container">
-                            <div className="row">
-                                <div className="col">
-                                    <h1>NuCamp</h1>
-                                    <h2>a better way to camp</h2>
-                                </div>
-                            </div>
-                        </div>
-                    </Jumbotron>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+          <ModalBody>
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  type="text"
+                  id="username"
+                  name="username"
+                  innerRef={(input) => (this.username = input)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  name="password"
+                  innerRef={(input) => (this.password = input)}
+                />
+              </FormGroup>
+              <FormGroup check>
+                <Label check>
+                  <Input
+                    type="checkbox"
+                    name="remember"
+                    innerRef={(input) => (this.remember = input)}
+                  />
+                  Remember me
+                </Label>
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">
+                Login
+              </Button>
+            </Form>
+          </ModalBody>
+        </Modal>
 
-                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
-                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
-                    <ModalBody>
-                        <Form onSubmit={this.handleLogin}>
-                            <FormGroup>
-                                <Label htmlFor="username">Username</Label>
-                                <Input type="text" id="username" name="username"
-                                    innerRef={input => this.username = input} />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label htmlFor="password">Password</Label>
-                                <Input type="password" id="password" name="password"
-                                    innerRef={input => this.password = input} />
-                            </FormGroup>
-                            <FormGroup check>
-                                <Label check>
-                                    <Input type="checkbox" name="remember"
-                                        innerRef={input => this.remember = input} />
-                                    Remember me
-                                </Label>
-                            </FormGroup>
-                            <Button type="submit" value="submit" color="primary">Login</Button>
-                        </Form>
-                    </ModalBody>
-                </Modal>
-
-                    <Navbar dark sticky="top" expand="md">
-                        <div className="container">
-                            <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="NuCamp Logo" /></NavbarBrand>
-                            <NavbarToggler onClick={this.toggleNav} />
-                            <Collapse isOpen={this.state.isNavOpen} navbar>
-                                <Nav navbar>
-                                    <NavItem>
-                                        <NavLink className="nav-link" to="/home">
-                                            <i className="fa fa-home fa-lg" /> Home
-                                        </NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink className="nav-link" to="/directory">
-                                            <i className="fa fa-list fa-lg" /> Directory
-                                        </NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink className="nav-link" to="/about">
-                                            <i className="fa fa-info fa-lg" /> About
-                                        </NavLink>
-                                    </NavItem>
-                                    <NavItem>
-                                        <NavLink className="nav-link" to="/contactus">
-                                            <i className="fa fa-address-card fa-lg" /> Contact Us
-                                        </NavLink>
-                                    </NavItem>
-                                </Nav>
-                                <span className="navbar-text ml-auto">
-                                <Button outline onClick={this.toggleModal}>
-                                    <i className="fa fa-sign-in fa-lg" /> Login
-                                </Button>
-                            </span>
-                            </Collapse>
-                        </div>
-                    </Navbar>
-                </React.Fragment>
-            );
-        }
+        <Navbar dark sticky="top" expand="md">
+          <div className="container">
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="/assets/images/logo.png"
+                height="30"
+                width="30"
+                alt="NuCamp Logo"
+              />
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggleNav} />
+            <Collapse isOpen={this.state.isNavOpen} navbar>
+              <Nav navbar>
+                <NavItem>
+                  <NavLink className="nav-link" to="/home">
+                    <i className="fa fa-home fa-lg" /> Home
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/directory">
+                    <i className="fa fa-list fa-lg" /> Directory
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/about">
+                    <i className="fa fa-info fa-lg" /> About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <i className="fa fa-address-card fa-lg" /> Contact Us
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <span className="navbar-text ml-auto">
+                <Loop in iterations={3.5}>
+                  <Fade in>
+                    <Button outline onClick={this.toggleModal}>
+                      <i className="fa fa-sign-in fa-lg" /> Login
+                    </Button>
+                  </Fade>
+                </Loop>
+              </span>
+            </Collapse>
+          </div>
+        </Navbar>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Header;
